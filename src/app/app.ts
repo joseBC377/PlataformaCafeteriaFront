d34725aa058d31562dd85a99146ac490618061e8
Daniela <danielasuarezfranklin.92@gmail.com>
2025-06-26 18:02:28 -0500

    feat: vista de inicio lista

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/header/header";
 import { Footer } from "./shared/footer/footer";
+import { Inicio } from "./pages/inicio/inicio";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Inicio],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
