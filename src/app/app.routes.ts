9ec54705db5e67575a81f9ca68ac52abce10e590
JoseBC <jbalcazar377@gmail.com>
2025-06-29 22:04:22 -0500

    actualizacion de menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Nosotros } from './pages/nosotros/nosotros';
 import { Contactanos } from './pages/contactanos/contactanos';
+import { Menu } from './pages/menu/menu';
 
 
 export const routes: Routes = [
   { path: '', component: Inicio },
   { path: 'nosotros', component: Nosotros },
   { path: 'contactanos', component: Contactanos },
+  {path:'menu', component:Menu, title:'Menu'}
 ];
