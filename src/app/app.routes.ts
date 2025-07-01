d314f83f08c18df63ff07851fb8bbf8680f38e0e
JoseBC <jbalcazar377@gmail.com>
2025-06-30 21:00:30 -0500

    creación de login y register responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,25 @@ import { Inicio } from './pages/inicio/inicio';
 import { Nosotros } from './pages/nosotros/nosotros';
 import { Contactanos } from './pages/contactanos/contactanos';
 import { Menu } from './pages/menu/menu';
+import { Login } from './pages/login/login';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
+import { PublicLayout } from './layouts/public-layout/public-layout';
 
 
 export const routes: Routes = [
-  { path: '', component: Inicio, title: 'inicio' },
-  { path: 'nosotros', component: Nosotros, title: 'nosotros' },
-  { path: 'contactanos', component: Contactanos, title: 'contactanos' },
-  { path: 'menu', component: Menu, title: 'Menu' }
+
+  {
+    path: 'admin', component: AdminLayout, children: [
+
+    ]
+  },
+  {
+    path: '', component: PublicLayout, children: [
+      { path: '', component: Inicio, title: 'inicio' },
+      { path: 'nosotros', component: Nosotros, title: 'nosotros' },
+      { path: 'contactanos', component: Contactanos, title: 'contactanos' },
+      { path: 'menu', component: Menu, title: 'Menu' },
+      { path: 'login', component: Login, title: 'login' }
+    ]
+  }
 ];
